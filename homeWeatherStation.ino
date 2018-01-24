1e5df1e56c39d0278d887433a833c1d04486ef10
Jonas Lemke <jns.lemke@gmail.com>
2018-01-24 12:43:53 +0100

    Changed indent to 4 spaces

--- homeWeatherStation.ino
+++ homeWeatherStation.ino
@@ -19,75 +19,75 @@ DHT dht(pinTempHum, DHTTYPE);
 bool sdOk = false;
 
 void setup() {
-  // Start serial connection
-  Serial.begin(9600);
-  
-  // Start and temperature/humidity sensor
-  dht.begin();
-  
-  // Start and setup display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.setTextSize(1.5);
-  display.setTextColor(WHITE);
-  
-  // Initialize SD card
-  pinMode(pinChipSelect, OUTPUT);
-  Serial.print("Initializing SD card...");
-  // See if the card is present and can be initialized
-  if (!SD.begin(pinChipSelect)) {
-    Serial.println("Card failed, or not present");
-    sdOk = false;
-  } else {
-    Serial.println("Card initialized.");
-    sdOk = true;
-  }
+    // Start serial connection
+    Serial.begin(9600);
+
+    // Start and temperature/humidity sensor
+    dht.begin();
+
+    // Start and setup display
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+
+    // Initialize SD card
+    pinMode(pinChipSelect, OUTPUT);
+    Serial.print("Initializing SD card...");
+    // See if the card is present and can be initialized
+    if (!SD.begin(pinChipSelect)) {
+        Serial.println("Card failed, or not present");
+        sdOk = false;
+    } else {
+        Serial.println("Card initialized.");
+        sdOk = true;
+    }
 }
 
 void loop() {
-  // Set update frequency
-  delay(1000);
+    // Set update frequency
+    delay(1000);
 
-  // Read values from sensor
-  float temp = dht.readTemperature();
-  float hum = dht.readHumidity();
+    // Read values from sensor
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
 
-  // Print sensor values
-  printSensorValues(temp, hum);
+    // Print sensor values
+    printSensorValues(temp, hum);
 
-  // Display sensor values
-  displaySensorValues(temp, hum);
-  
-  // Write values to SD card
-  if (sdOk) {
-//    writeToSd(temp, hum);
-  }
+    // Display sensor values
+    displaySensorValues(temp, hum);
+
+    // Write values to SD card
+    if (sdOk) {
+//        writeToSd(temp, hum);
+    }
 }
 
 void printSensorValues(float temp, float hum) {
-  Serial.print("Temperature: "); Serial.print(temp); Serial.println("°C");
-  Serial.print("Humidity: "); Serial.print(hum); Serial.println("%");
+    Serial.print("Temperature: "); Serial.print(temp); Serial.println("°C");
+    Serial.print("Humidity: "); Serial.print(hum); Serial.println("%");
 }
 
 void displaySensorValues(float temp, float hum) {
-  display.clearDisplay();
-  display.setTextSize(1.5);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.print("Temperature: "); display.print(temp); 
-  display.print((char)247); display.println("C");
-  display.print("Humidity: "); display.print(hum); display.println("%");
-  display.display();
+    display.clearDisplay();
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.print("Temperature: "); display.print(temp); 
+    display.print((char)247); display.println("C");
+    display.print("Humidity: "); display.print(hum); display.println("%");
+    display.display();
 }
 
 void writeToSd(float temp, float hum) {
-  String dataString = String(temp) + "," + String(hum);
-  File dataFile = SD.open("humTempLog.txt", FILE_WRITE);
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    Serial.print("Data string: "); Serial.println(dataString);
-  } else {
-    Serial.println("Error opening humTempLog.txt");
-  }
+    String dataString = String(temp) + "," + String(hum);
+    File dataFile = SD.open("humTempLog.txt", FILE_WRITE);
+    if (dataFile) {
+        dataFile.println(dataString);
+        dataFile.close();
+        Serial.print("Data string: "); Serial.println(dataString);
+    } else {
+        Serial.println("Error opening humTempLog.txt");
+    }
 }
 
